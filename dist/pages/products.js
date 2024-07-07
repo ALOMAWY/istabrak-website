0c8672358a33fc841c1708ab12314ca30e753197
Abdalrahman Aldabbas <aldabbas333abdalrahman@gmail.com>
2024-07-07 07:54:10 +0300

    edit products page with js

--- dist/pages/products.js
+++ dist/pages/products.js
@@ -1,135 +1,159 @@
 "use strict";
-let productsImages = document.querySelectorAll(".products .img-holder img");
-let productsCard1 = document.querySelectorAll(".products .container-75 .holder-1 .row div");
-let productsCard2 = document.querySelectorAll(".products .container-75 .holder-2 .row div");
-let showMore_1 = document.querySelector(".products .container-75 .holder-1 .show-more");
-let showMore_2 = document.querySelector(".products .container-75 .holder-2 .show-more");
-let pCards1 = Array.from(productsCard1);
-let pCards2 = Array.from(productsCard2);
-let productsSizeLabels = document.querySelectorAll(".products-size");
-let productsSizeLabel_1 = document.querySelector(".products-size-1");
-let productsSizeLabel_2 = document.querySelector(".products-size-2");
-productsSizeLabels.forEach((e) => {
-    let ele = e;
-    let productsParent = document.querySelector(ele.dataset.size || "");
-    ele.innerHTML = `${productsParent === null || productsParent === void 0 ? void 0 : productsParent.children.length} All Items` || "0 Items ";
-});
-function cardsFlowControl(cards, showMore, sizeLabel) {
-    let startShowingCards = cards === null || cards === void 0 ? void 0 : cards.filter((card) => !card.classList.contains("d-none")).length;
-    let scaleElementNumber = startShowingCards + 4;
-    if (cards.length > startShowingCards) {
-        showMore.classList.remove("d-none");
-        cards.forEach((elem, index) => {
-            if (index >= startShowingCards && index < scaleElementNumber) {
-                function scalingShow(ele) {
-                    ele.style.scale = "0 1";
-                    ele.classList.remove("d-none");
-                    setTimeout(() => {
-                        ele.style.scale = "1 1";
-                    }, 0);
-                }
-                scalingShow(elem);
-            }
-            if (scaleElementNumber >= cards.length) {
-                showMore.classList.add("d-none");
-            }
-        });
-    }
-    else {
-        showMore.classList.add("d-none");
-    }
-    let endShowingCards = cards === null || cards === void 0 ? void 0 : cards.filter((card) => !card.classList.contains("d-none")).length;
-    let productsParent = document.querySelector(sizeLabel.dataset.size || "");
-    sizeLabel.innerHTML =
-        `${endShowingCards} Of ${productsParent === null || productsParent === void 0 ? void 0 : productsParent.children.length}` || "0 Items ";
-}
-let cardsList = [Array.from(productsCard1), Array.from(productsCard2)];
-let gallery = document.getElementById("gallery");
-let selectedImage = document.getElementById("selected-image");
-[...pCards1, ...pCards2].forEach((crd) => {
-    let card = crd;
-    let cardWidth = card.clientWidth;
-    card.style.height = cardWidth + "px";
-    card.addEventListener("click", () => {
-        let img = card.firstElementChild;
-        gallery.classList.remove("d-none");
-        gallery.classList.add("d-flex");
-        imageContainer.style.maxWidth = imageContainer.clientWidth + "px";
-        imageContainer.style.maxHeight = imageContainer.clientHeight + "px";
-        imageContainer.style.overflow = "hidden";
-        selectedImage.src = img.src;
-        let scale = 1;
-        let originX = 0;
-        let originY = 0;
-        selectedImage.addEventListener("wheel", (event) => {
-            event.preventDefault();
-            scale += event.deltaY * -0.01;
-            scale = Math.min(Math.max(1, scale), 3);
-            selectedImage.style.transform = `scale(${scale}) translate(${originX}px, ${originY}px)`;
-        });
-        let isDragging = false;
-        let startX, startY;
-        selectedImage.addEventListener("mousedown", (event) => {
-            isDragging = true;
-            startX = event.clientX;
-            startY = event.clientY;
-            selectedImage.style.cursor = "grabbing";
-        });
-        selectedImage.addEventListener("mouseup", () => {
-            isDragging = false;
-            selectedImage.style.cursor = "grab";
-        });
-        selectedImage.addEventListener("mousemove", (event) => {
-            if (isDragging) {
-                let dx = (event.clientX - startX) / scale;
-                let dy = (event.clientY - startY) / scale;
-                originX += dx;
-                originY += dy;
-                startX = event.clientX;
-                startY = event.clientY;
-                selectedImage.style.transform = `scale(${scale}) translate(${originX}px, ${originY}px)`;
-            }
-        });
-        fullScreenImage === null || fullScreenImage === void 0 ? void 0 : fullScreenImage.addEventListener("click", () => {
-            console.log("fullscreen");
-            gallery.requestFullscreen();
-            selectedImage.style.maxWidth = "90%";
-        });
-        zoomInImage === null || zoomInImage === void 0 ? void 0 : zoomInImage.addEventListener("click", () => {
-            let currentScale = parseInt(window.getComputedStyle(selectedImage).scale);
-            if (currentScale <= 10) {
-                selectedImage.style.scale = ` ${currentScale * 2}`;
-            }
-        });
-        zoomOutImage === null || zoomOutImage === void 0 ? void 0 : zoomOutImage.addEventListener("click", () => {
-            let currentScale = parseInt(window.getComputedStyle(selectedImage).scale);
-            if (currentScale > 1) {
-                // selectedImage.style.scale = ` ${currentScale / 2}`;
-            }
-        });
-    });
-});
-let galleryCloseButton = document.querySelector(".gallery .close-gallery");
-galleryCloseButton === null || galleryCloseButton === void 0 ? void 0 : galleryCloseButton.addEventListener("click", () => {
-    gallery.classList.remove("d-flex");
-    gallery.classList.add("d-none");
-});
+let products = Array.from(document.querySelectorAll(".product"));
 let imageContainer = document.querySelector(".gallery .image");
 let toolsBox = document.querySelector(".gallery .tools");
 let fullScreenImage = document.querySelector(".gallery .tools .full-screen");
 let zoomInImage = document.querySelector(".gallery .tools .zoom-in");
 let zoomOutImage = document.querySelector(".gallery .tools .zoom-out");
-cardsList.forEach((crds) => {
-    crds.forEach((crd, index) => {
-        let card = crd;
+let closeGallery = document.querySelector(".gallery .tools .close");
+let share = document.querySelector(".gallery .tools .share");
+let whatsappShare = document.querySelector(".gallery .tools .whatsapp");
+products.forEach((ele, index) => {
+    let prod = ele;
+    let showMore = document.querySelector(`${prod.dataset.showmore}`);
+    let showLimit = 4;
+    let productsHolder = Array.from(document.querySelectorAll(`.holder-${index + 1} .row > div`));
+    showMore === null || showMore === void 0 ? void 0 : showMore.addEventListener("click", () => {
+        let productsHolder = Array.from(document.querySelectorAll(`.holder-${index + 1} .row > div`));
+        let startShowingCards = Array.from(productsHolder).filter((crd) => {
+            let card = crd;
+            return !card.classList.contains("d-none");
+        }).length;
+        if (startShowingCards < productsHolder.length) {
+            productsHolder.forEach((e, i) => {
+                if (i >= startShowingCards && i < startShowingCards + showLimit) {
+                    scalingShow(e);
+                    if (startShowingCards + showLimit >= productsHolder.length)
+                        showMore === null || showMore === void 0 ? void 0 : showMore.classList.add("d-none");
+                }
+            });
+        }
+        else {
+            showMore === null || showMore === void 0 ? void 0 : showMore.classList.add("d-none");
+        }
+        let endShowingCards = Array.from(productsHolder).filter((crd) => {
+            let card = crd;
+            return !card.classList.contains("d-none");
+        }).length;
+        let sizeLabel = document.querySelector(`${prod.dataset.sizelabel}`);
+        if (sizeLabel) {
+            sizeLabel.innerHTML =
+                `${endShowingCards} Of ${productsHolder.length}` || "0 Items ";
+        }
+    });
+    productsHolder.forEach((card, index) => {
         if (index >= 8) {
             card.classList.add("d-none");
         }
     });
+    let sizeLabel = document.querySelector(`${prod.dataset.sizelabel}`);
+    if (sizeLabel) {
+        sizeLabel.innerHTML =
+            ` All Items Is : ${productsHolder.length}` || "0 Items ";
+    }
+    productsHolder.forEach((card) => {
+        card.addEventListener("click", () => {
+            gallery.classList.remove("d-none");
+            gallery.classList.add("d-flex");
+            let clickedImage = card.children[0];
+            if (clickedImage.tagName == "IMG")
+                selectedImage.src = clickedImage.src;
+        });
+        // sh?.addEventListener("click",()=>{})
+    });
 });
-showMore_1.addEventListener("click", () => {
-    cardsFlowControl(pCards1, showMore_1, productsSizeLabel_1);
+let productsImages = document.querySelectorAll(".products .img-holder img");
+let gallery = document.getElementById("gallery");
+let selectedImage = document.getElementById("selected-image");
+let galleryCloseButton = document.querySelector(".gallery .close-gallery");
+galleryCloseButton === null || galleryCloseButton === void 0 ? void 0 : galleryCloseButton.addEventListener("click", () => {
+    gallery.classList.remove("d-flex");
+    gallery.classList.add("d-none");
+    document.exitFullscreen();
 });
-showMore_2.addEventListener("click", () => {
-    cardsFlowControl(pCards2, showMore_2, productsSizeLabel_2);
+fullScreenImage === null || fullScreenImage === void 0 ? void 0 : fullScreenImage.addEventListener("click", () => {
+    if (fullScreenImage.dataset.fullscreen == "true") {
+        fullScreenImage.dataset.fullscreen = "false";
+        fullScreenImage.innerHTML = `<i class="fa-solid fa-expand"></i>`;
+        document.exitFullscreen();
+    }
+    else {
+        fullScreenImage.dataset.fullscreen = "true";
+        gallery.requestFullscreen();
+        fullScreenImage.innerHTML = `<i class="fa-solid fa-compress"></i>`;
+    }
 });
+zoomInImage === null || zoomInImage === void 0 ? void 0 : zoomInImage.addEventListener("click", () => { });
+zoomOutImage === null || zoomOutImage === void 0 ? void 0 : zoomOutImage.addEventListener("click", () => { });
+function scalingShow(ele) {
+    ele.style.scale = "0 1";
+    ele.classList.remove("d-none");
+    setTimeout(() => {
+        ele.style.scale = "1 1";
+    }, 0);
+}
+// window.addEventListener("scroll", () => {
+//   let scrollY = window.scrollY;
+//   let offsetTop = title.offsetTop;
+//   if (scrollY >= offsetTop && scrollY < offsetTop.parent.clientHeight) {
+//     title.classList.add("position-fixed");
+//   }
+// });
+// card.addEventListener("click", () => {
+//   let img = card.firstElementChild as HTMLImageElement;
+//   gallery.classList.remove("d-none");
+//   gallery.classList.add("d-flex");
+//   imageContainer.style.maxWidth = "75%";
+//   // imageContainer.style.maxHeight = imageContainer.clientHeight + "px";
+//   imageContainer.style.overflow = "hidden";
+//   selectedImage.src = img.src;
+//   let scale = 1;
+//   let originX = 0;
+//   let originY = 0;
+//   selectedImage.addEventListener("wheel", (event) => {
+//     event.preventDefault();
+//     scale += event.deltaY * -0.01;
+//     scale = Math.min(Math.max(1, scale), 3);
+//     selectedImage.style.transform = `scale(${scale}) translate(${originX}px, ${originY}px)`;
+//   });
+//   let isDragging = false;
+//   let startX: number, startY: number;
+//   selectedImage.addEventListener("mousedown", (event) => {
+//     isDragging = true;
+//     startX = event.clientX;
+//     startY = event.clientY;
+//     selectedImage.style.cursor = "grabbing";
+//   });
+//   selectedImage.addEventListener("mouseup", () => {
+//     isDragging = false;
+//     selectedImage.style.cursor = "grab";
+//   });
+//   selectedImage.addEventListener("mousemove", (event) => {
+//     if (isDragging) {
+//       let dx = (event.clientX - startX) / scale;
+//       let dy = (event.clientY - startY) / scale;
+//       originX += dx;
+//       originY += dy;
+//       startX = event.clientX;
+//       startY = event.clientY;
+//       selectedImage.style.transform = `scale(${scale}) translate(${originX}px, ${originY}px)`;
+//     }
+//   });
+//   fullScreenImage?.addEventListener("click", () => {
+//     console.log("fullscreen");
+//     gallery.requestFullscreen();
+//     selectedImage.style.maxWidth = "90%";
+//   });
+//   zoomInImage?.addEventListener("click", () => {
+//     let currentScale = parseInt(window.getComputedStyle(selectedImage).scale);
+//     if (currentScale <= 10) {
+//       selectedImage.style.scale = ` ${currentScale * 2}`;
+//     }
+//   });
+//   zoomOutImage?.addEventListener("click", () => {
+//     let currentScale = parseInt(window.getComputedStyle(selectedImage).scale);
+//     if (currentScale > 1) {
+//       // selectedImage.style.scale = ` ${currentScale / 2}`;
+//     }
+//   });
+// });
--- js/pages/products.ts
+++ js/pages/products.ts
@@ -1,84 +1,97 @@
-let productsImages = document.querySelectorAll(".products .img-holder img");
+let products = Array.from(document.querySelectorAll(".product"));
 
-let productsCard1 = document.querySelectorAll(
-  ".products .container-75 .holder-1 .row div"
-);
-let productsCard2 = document.querySelectorAll(
-  ".products .container-75 .holder-2 .row div"
-);
+let imageContainer = document.querySelector(".gallery .image") as HTMLElement;
 
-let showMore_1 = document.querySelector(
-  ".products .container-75 .holder-1 .show-more"
-) as HTMLElement;
-let showMore_2 = document.querySelector(
-  ".products .container-75 .holder-2 .show-more"
+let toolsBox = document.querySelector(".gallery .tools");
+
+let fullScreenImage = document.querySelector(
+  ".gallery .tools .full-screen"
 ) as HTMLElement;
 
-let pCards1 = Array.from(productsCard1);
+let zoomInImage = document.querySelector(".gallery .tools .zoom-in");
 
-let pCards2 = Array.from(productsCard2);
+let zoomOutImage = document.querySelector(".gallery .tools .zoom-out");
 
-let productsSizeLabels = document.querySelectorAll(".products-size");
+let closeGallery = document.querySelector(".gallery .tools .close");
 
-let productsSizeLabel_1 = document.querySelector(
-  ".products-size-1"
-) as HTMLElement;
+let share = document.querySelector(".gallery .tools .share");
 
-let productsSizeLabel_2 = document.querySelector(
-  ".products-size-2"
-) as HTMLElement;
+let whatsappShare = document.querySelector(".gallery .tools .whatsapp");
 
-productsSizeLabels.forEach((e) => {
-  let ele = e as HTMLElement;
-  let productsParent = document.querySelector(ele.dataset.size || "");
+products.forEach((ele: Element, index: number) => {
+  let prod = ele as HTMLElement;
 
-  ele.innerHTML = `${productsParent?.children.length} All Items` || "0 Items ";
+  let showMore = document.querySelector(`${prod.dataset.showmore}`);
+
+  let showLimit = 4;
+
+  let productsHolder = Array.from(
+    document.querySelectorAll(`.holder-${index + 1} .row > div`)
+  );
+
+  showMore?.addEventListener("click", () => {
+    let productsHolder = Array.from(
+      document.querySelectorAll(`.holder-${index + 1} .row > div`)
+    );
+
+    let startShowingCards = Array.from(productsHolder).filter((crd) => {
+      let card = crd;
+      return !card.classList.contains("d-none");
+    }).length;
+
+    if (startShowingCards < productsHolder.length) {
+      productsHolder.forEach((e, i) => {
+        if (i >= startShowingCards && i < startShowingCards + showLimit) {
+          scalingShow(e as HTMLElement);
+
+          if (startShowingCards + showLimit >= productsHolder.length)
+            showMore?.classList.add("d-none");
+        }
+      });
+    } else {
+      showMore?.classList.add("d-none");
+    }
+
+    let endShowingCards = Array.from(productsHolder).filter((crd) => {
+      let card = crd;
+      return !card.classList.contains("d-none");
+    }).length;
+
+    let sizeLabel = document.querySelector(`${prod.dataset.sizelabel}`);
+    if (sizeLabel) {
+      sizeLabel.innerHTML =
+        `${endShowingCards} Of ${productsHolder.length}` || "0 Items ";
+    }
+  });
+
+  productsHolder.forEach((card, index) => {
+    if (index >= 8) {
+      card.classList.add("d-none");
+    }
+  });
+
+  let sizeLabel = document.querySelector(`${prod.dataset.sizelabel}`);
+
+  if (sizeLabel) {
+    sizeLabel.innerHTML =
+      ` All Items Is : ${productsHolder.length}` || "0 Items ";
+  }
+
+  productsHolder.forEach((card) => {
+    card.addEventListener("click", () => {
+      gallery.classList.remove("d-none");
+      gallery.classList.add("d-flex");
+
+      let clickedImage = card.children[0] as HTMLImageElement;
+
+      if (clickedImage.tagName == "IMG") selectedImage.src = clickedImage.src;
+    });
+
+    // sh?.addEventListener("click",()=>{})
+  });
 });
 
-function cardsFlowControl(
-  cards: Element[],
-  showMore: HTMLElement,
-  sizeLabel: HTMLElement
-): void {
-  let startShowingCards = cards?.filter(
-    (card: Element) => !card.classList.contains("d-none")
-  ).length;
-
-  let scaleElementNumber: number = startShowingCards + 4;
-
-  if (cards.length > startShowingCards) {
-    showMore.classList.remove("d-none");
-
-    cards.forEach((elem, index: number) => {
-      if (index >= startShowingCards && index < scaleElementNumber) {
-        function scalingShow(ele: HTMLElement) {
-          ele.style.scale = "0 1";
-          ele.classList.remove("d-none");
-          setTimeout(() => {
-            ele.style.scale = "1 1";
-          }, 0);
-        }
-        scalingShow(elem as HTMLElement);
-      }
-
-      if (scaleElementNumber >= cards.length) {
-        showMore.classList.add("d-none");
-      }
-    });
-  } else {
-    showMore.classList.add("d-none");
-  }
-  let endShowingCards = cards?.filter(
-    (card: Element) => !card.classList.contains("d-none")
-  ).length;
-
-  let productsParent = document.querySelector(sizeLabel.dataset.size || "");
-
-  sizeLabel.innerHTML =
-    `${endShowingCards} Of ${productsParent?.children.length}` || "0 Items ";
-}
-
-let cardsList = [Array.from(productsCard1), Array.from(productsCard2)];
+let productsImages = document.querySelectorAll(".products .img-holder img");
 
 let gallery = document.getElementById("gallery") as HTMLElement;
 
@@ -86,120 +99,124 @@ let selectedImage = document.getElementById(
   "selected-image"
 ) as HTMLImageElement;
 
-[...pCards1, ...pCards2].forEach((crd) => {
-  let card = crd as HTMLElement;
-
-  let cardWidth = card.clientWidth;
-
-  card.style.height = cardWidth + "px";
-
-  card.addEventListener("click", () => {
-    let img = card.firstElementChild as HTMLImageElement;
-
-    gallery.classList.remove("d-none");
-
-    gallery.classList.add("d-flex");
-
-    imageContainer.style.maxWidth = imageContainer.clientWidth + "px";
-
-    imageContainer.style.maxHeight = imageContainer.clientHeight + "px";
-
-    imageContainer.style.overflow = "hidden";
-
-    selectedImage.src = img.src;
-
-    let scale = 1;
-    let originX = 0;
-    let originY = 0;
-
-    selectedImage.addEventListener("wheel", (event) => {
-      event.preventDefault();
-      scale += event.deltaY * -0.01;
-      scale = Math.min(Math.max(1, scale), 3);
-      selectedImage.style.transform = `scale(${scale}) translate(${originX}px, ${originY}px)`;
-    });
-
-    let isDragging = false;
-    let startX: number, startY: number;
-
-    selectedImage.addEventListener("mousedown", (event) => {
-      isDragging = true;
-      startX = event.clientX;
-      startY = event.clientY;
-      selectedImage.style.cursor = "grabbing";
-    });
-
-    selectedImage.addEventListener("mouseup", () => {
-      isDragging = false;
-      selectedImage.style.cursor = "grab";
-    });
-
-    selectedImage.addEventListener("mousemove", (event) => {
-      if (isDragging) {
-        let dx = (event.clientX - startX) / scale;
-        let dy = (event.clientY - startY) / scale;
-        originX += dx;
-        originY += dy;
-        startX = event.clientX;
-        startY = event.clientY;
-        selectedImage.style.transform = `scale(${scale}) translate(${originX}px, ${originY}px)`;
-      }
-    });
-
-    fullScreenImage?.addEventListener("click", () => {
-      console.log("fullscreen");
-      gallery.requestFullscreen();
-      selectedImage.style.maxWidth = "90%";
-    });
-
-    zoomInImage?.addEventListener("click", () => {
-      let currentScale = parseInt(window.getComputedStyle(selectedImage).scale);
-
-      if (currentScale <= 10) {
-        selectedImage.style.scale = ` ${currentScale * 2}`;
-      }
-    });
-
-    zoomOutImage?.addEventListener("click", () => {
-      let currentScale = parseInt(window.getComputedStyle(selectedImage).scale);
-
-      if (currentScale > 1) {
-        // selectedImage.style.scale = ` ${currentScale / 2}`;
-      }
-    });
-  });
-});
-
 let galleryCloseButton = document.querySelector(".gallery .close-gallery");
 
 galleryCloseButton?.addEventListener("click", () => {
   gallery.classList.remove("d-flex");
+
   gallery.classList.add("d-none");
+
+  document.exitFullscreen();
 });
 
-let imageContainer = document.querySelector(".gallery .image") as HTMLElement;
+fullScreenImage?.addEventListener("click", () => {
+  if (fullScreenImage.dataset.fullscreen == "true") {
+    fullScreenImage.dataset.fullscreen = "false";
 
-let toolsBox = document.querySelector(".gallery .tools");
+    fullScreenImage.innerHTML = `<i class="fa-solid fa-expand"></i>`;
 
-let fullScreenImage = document.querySelector(".gallery .tools .full-screen");
+    document.exitFullscreen();
+  } else {
+    fullScreenImage.dataset.fullscreen = "true";
 
-let zoomInImage = document.querySelector(".gallery .tools .zoom-in");
+    gallery.requestFullscreen();
 
-let zoomOutImage = document.querySelector(".gallery .tools .zoom-out");
-
-cardsList.forEach((crds) => {
-  crds.forEach((crd, index) => {
-    let card = crd as HTMLElement;
-
-    if (index >= 8) {
-      card.classList.add("d-none");
-    }
-  });
+    fullScreenImage.innerHTML = `<i class="fa-solid fa-compress"></i>`;
+  }
 });
+zoomInImage?.addEventListener("click", () => {});
+zoomOutImage?.addEventListener("click", () => {});
 
-showMore_1.addEventListener("click", () => {
-  cardsFlowControl(pCards1, showMore_1, productsSizeLabel_1);
-});
-showMore_2.addEventListener("click", () => {
-  cardsFlowControl(pCards2, showMore_2, productsSizeLabel_2);
-});
+function scalingShow(ele: HTMLElement) {
+  ele.style.scale = "0 1";
+  ele.classList.remove("d-none");
+  setTimeout(() => {
+    ele.style.scale = "1 1";
+  }, 0);
+}
+
+// window.addEventListener("scroll", () => {
+//   let scrollY = window.scrollY;
+
+//   let offsetTop = title.offsetTop;
+
+//   if (scrollY >= offsetTop && scrollY < offsetTop.parent.clientHeight) {
+//     title.classList.add("position-fixed");
+//   }
+// });
+
+// card.addEventListener("click", () => {
+//   let img = card.firstElementChild as HTMLImageElement;
+
+//   gallery.classList.remove("d-none");
+
+//   gallery.classList.add("d-flex");
+
+//   imageContainer.style.maxWidth = "75%";
+
+//   // imageContainer.style.maxHeight = imageContainer.clientHeight + "px";
+
+//   imageContainer.style.overflow = "hidden";
+
+//   selectedImage.src = img.src;
+
+//   let scale = 1;
+//   let originX = 0;
+//   let originY = 0;
+
+//   selectedImage.addEventListener("wheel", (event) => {
+//     event.preventDefault();
+//     scale += event.deltaY * -0.01;
+//     scale = Math.min(Math.max(1, scale), 3);
+//     selectedImage.style.transform = `scale(${scale}) translate(${originX}px, ${originY}px)`;
+//   });
+
+//   let isDragging = false;
+//   let startX: number, startY: number;
+
+//   selectedImage.addEventListener("mousedown", (event) => {
+//     isDragging = true;
+//     startX = event.clientX;
+//     startY = event.clientY;
+//     selectedImage.style.cursor = "grabbing";
+//   });
+
+//   selectedImage.addEventListener("mouseup", () => {
+//     isDragging = false;
+//     selectedImage.style.cursor = "grab";
+//   });
+
+//   selectedImage.addEventListener("mousemove", (event) => {
+//     if (isDragging) {
+//       let dx = (event.clientX - startX) / scale;
+//       let dy = (event.clientY - startY) / scale;
+//       originX += dx;
+//       originY += dy;
+//       startX = event.clientX;
+//       startY = event.clientY;
+//       selectedImage.style.transform = `scale(${scale}) translate(${originX}px, ${originY}px)`;
+//     }
+//   });
+
+//   fullScreenImage?.addEventListener("click", () => {
+//     console.log("fullscreen");
+//     gallery.requestFullscreen();
+//     selectedImage.style.maxWidth = "90%";
+//   });
+
+//   zoomInImage?.addEventListener("click", () => {
+//     let currentScale = parseInt(window.getComputedStyle(selectedImage).scale);
+
+//     if (currentScale <= 10) {
+//       selectedImage.style.scale = ` ${currentScale * 2}`;
+//     }
+//   });
+
+//   zoomOutImage?.addEventListener("click", () => {
+//     let currentScale = parseInt(window.getComputedStyle(selectedImage).scale);
+
+//     if (currentScale > 1) {
+//       // selectedImage.style.scale = ` ${currentScale / 2}`;
+//     }
+//   });
+// });
